331ee2074f9ab9349ec661424268705d22e82ed1
Andy Patari <andy.patari@gmail.com>
2023-07-17 14:04:43 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (drivers) => [drivers[0], drivers[1]]
+
+const returnLastTwoDrivers = (drivers) => [drivers[drivers.length - 2], drivers[drivers.length - 1]]
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(n) {
+    return(fare => fare * n)
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, funForSelecting) => funForSelecting(drivers)
